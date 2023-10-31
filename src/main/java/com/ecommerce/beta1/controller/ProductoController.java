eb23feb10a7e72efaece891cdcf3846a227e2854
otraVezLucho <otrodeluisvillada@gmail.com>
2023-10-30 22:42:49 -0500

    error de la variable logger solucionado por fin, proceso documentado, codigo funcional

--- src/main/java/com/ecommerce/beta1/controller/ProductoController.java
+++ src/main/java/com/ecommerce/beta1/controller/ProductoController.java
@@ -1,20 +1,21 @@
 package com.ecommerce.beta1.controller;
 
 
+import com.ecommerce.beta1.model.Producto;
 import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.logging.Logger;
+//import java.util.logging.Logger; cuando se usa org.slf4j.* no se usa esta por eso la comento
 
 @Controller
 
 @RequestMapping("/productos")
 public class ProductoController {
 
-    // private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class); //busca por nombre de la clase
 
     @GetMapping("") // Esta anotacion no tiene ningun valor
     public String show(){
@@ -25,6 +26,12 @@ public class ProductoController {
     @GetMapping("/create")
     public String create(){
         return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("este es el objeto producto {}",producto);
+        return "redirect:/productos";
     }
 
 }
