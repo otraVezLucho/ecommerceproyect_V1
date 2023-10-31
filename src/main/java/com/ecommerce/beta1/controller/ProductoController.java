7b9b4dca88f364acc5585ffe6dcde8a0ba583db5
otraVezLucho <otrodeluisvillada@gmail.com>
2023-10-30 22:38:12 -0500

    se implemento la pagina para crear producto, el drop down superior de ADMIN

--- src/main/java/com/ecommerce/beta1/controller/ProductoController.java
+++ src/main/java/com/ecommerce/beta1/controller/ProductoController.java
@@ -1,16 +1,30 @@
 package com.ecommerce.beta1.controller;
 
-import jakarta.persistence.Table;
+
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
+
 @RequestMapping("/productos")
 public class ProductoController {
+
+    //private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 
     @GetMapping("") // Esta anotacion no tiene ningun valor
     public String show(){
         return "productos/show";
     }
+
+
+    @GetMapping("/create")
+    public String create(){
+        return "productos/create";
+    }
+
 }
