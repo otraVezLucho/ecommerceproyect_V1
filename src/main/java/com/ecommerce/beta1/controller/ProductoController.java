4cfa7c3fcb9fa09e0cdac72d2f493c1a04679759
otraVezLucho <otrodeluisvillada@gmail.com>
2023-11-21 23:10:28 -0500

    Se creo la funcionalidad de guardar el producto enlazada a la pagina de productos, se utiliza LOOGER para realizar pruebas con la consola y se implemento maualmente un usuario a la base de datos para poder asignarlo y poder guardar correctamente con la interfaz productoService.save y lograr completar una parte del CRUD

--- src/main/java/com/ecommerce/beta1/controller/ProductoController.java
+++ src/main/java/com/ecommerce/beta1/controller/ProductoController.java
@@ -2,7 +2,10 @@ package com.ecommerce.beta1.controller;
 
 
 import com.ecommerce.beta1.model.Producto;
+import com.ecommerce.beta1.model.Usuario;
+import com.ecommerce.beta1.service.ProductoService;
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,8 @@ public class ProductoController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class); //busca por nombre de la clase
 
+    @Autowired // para que no haya que crear manualmente un objeto sin tener que instanciarlo y que lo haga el mismo contenedor de Spring
+    private ProductoService productoService;
     @GetMapping("") // Esta anotacion no tiene ningun valor
     public String show(){
         return "productos/show";
@@ -30,7 +35,11 @@ public class ProductoController {
 
     @PostMapping("/save")
     public String save(Producto producto){
-        LOGGER.info("este es el objeto producto {}",producto);
+        LOGGER.info("este es el objeto producto {}",producto); // El metodo toString de la entidad Producto es lo que se va a mostrar en consola si no hay error y esa es la funcion de LOOGER
+        // Antes de hacer el guardado se necesita crear un usuario de prueba y asignarlo para que no salga error porque no hay usuario asignado
+        Usuario usuario1 = new Usuario(1,"","","","","","","");
+        producto.setUsuario(usuario1);
+        productoService.save(producto);
         return "redirect:/productos";
     }
 
