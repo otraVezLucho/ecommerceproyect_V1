2c9062c9f948fb7e8bf1f2449aa1c61da2171ce6
otraVezLucho <otrodeluisvillada@gmail.com>
2023-12-19 23:32:42 -0500

    se crea template home para usuario y se crea HomeControler para mostrar los productos en la pagina home del usuario

--- src/main/java/com/ecommerce/beta1/controller/HomeController.java
+++ src/main/java/com/ecommerce/beta1/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.ecommerce.beta1.controller;
 
+import com.ecommerce.beta1.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/") // la linea sola indica que se debe dirigir a la raiz
 public class HomeController {
 
+    @Autowired//para que inyecte el contenedor del framework
+    private ProductoService productoService; // para obtener los productos
+
     @GetMapping("")
-    public String home (){
+    public String home (Model model){
+
+        model.addAttribute("productos", productoService.findAll() ); // productoService.findAll() va a traer todos los productos para colocarlos en la variable productos
         return "usuario/home";
     }
 }
