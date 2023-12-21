4722ebab64d6e1bfccd2e4b8050d829447abb786
otraVezLucho <otrodeluisvillada@gmail.com>
2023-12-20 22:25:00 -0500

    se agrego el archivo productohome que es la vista detallada del producto, se agrego la logica en HomeController para poder redirigir a la pagina productohome pero aun no se ha creado la logica para traer la informacion del producto (CODIGO FUNCIONAL)

--- src/main/java/com/ecommerce/beta1/controller/HomeController.java
+++ src/main/java/com/ecommerce/beta1/controller/HomeController.java
@@ -1,15 +1,23 @@
 package com.ecommerce.beta1.controller;
 
 import com.ecommerce.beta1.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 @RequestMapping("/") // la linea sola indica que se debe dirigir a la raiz
 public class HomeController {
+
+    //Importante usar el paquete de slf4j
+    private final Logger log = LoggerFactory.getLogger(HomeController.class); //imprime el objeto solicitado en consola
 
     @Autowired//para que inyecte el contenedor del framework
     private ProductoService productoService; // para obtener los productos
@@ -19,5 +27,11 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll() ); // productoService.findAll() va a traer todos los productos para colocarlos en la variable productos
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}") // Indica que va a obtener la informacion de solo el producto que indica la variable id
+    public String productoHome(@PathVariable Integer id){
+        log.info("LOGGER FUNCIONANDO: Id producto enviado como parametro {}",id);
+        return"usuario/productohome";
     }
 }
