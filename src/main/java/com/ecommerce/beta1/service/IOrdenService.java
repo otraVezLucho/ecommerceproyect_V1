002c2e8e627a2ff780f4554491c64f141826880d
otraVezLucho <otrodeluisvillada@gmail.com>
2024-01-03 21:35:01 -0500

    se crea metodo para generar el numero de la orden en la clase OrdenServiceImp

--- src/main/java/com/ecommerce/beta1/service/IOrdenService.java
+++ src/main/java/com/ecommerce/beta1/service/IOrdenService.java
@@ -2,10 +2,12 @@ package com.ecommerce.beta1.service;
 
 import com.ecommerce.beta1.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
 
-    Orden save(Orden orden);
+    Orden save(Orden orden); //Metodo de Interfaz para guardar una orden
 
-
+    List<Orden> findAll(); // Metodo de interfaz para obtener una lista de ordenes o buscar una lista de ordenes
 
 }
--- src/main/java/com/ecommerce/beta1/service/OrdenServiceImp.java
+++ src/main/java/com/ecommerce/beta1/service/OrdenServiceImp.java
@@ -4,6 +4,9 @@ import com.ecommerce.beta1.model.Orden;
 import com.ecommerce.beta1.repository.IOrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrdenServiceImp implements IOrdenService{
 
     @Autowired
@@ -13,5 +16,43 @@ public class OrdenServiceImp implements IOrdenService{
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
 
+    }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+
+    }
+
+    //Revisar V.32
+    public String GeneradorNumeroOrden(){
+        int numero=0;
+        String numeroConcatenado="";//devuelve el string con el secuecial de la orden
+
+        List<Orden> ordenes =findAll(); // la idea es obtener todas la ordenes y obtener el ultimo numero ingresado de esa orden
+        List <Integer> numeros = new ArrayList<Integer>(); // en esta lista se va a colocar los numeros de las ordenes de la lista ordenes, se debe hacer cast a numeros porque la lista ordenes es de strings
+        ordenes.stream().forEach(o-> numeros.add(Integer.parseInt(o.getNumero())));// se pasa el numero de orden de que viene de la lista ordenes, se recorre la lista y se agregan los numeros a la lista 'numeros' pero en formato int
+
+        //Obtener el ultimo numero de la orden para saber cual debe ser el siguiente numero de la orden
+        if (ordenes.isEmpty()){
+             numero =1;
+        } else{
+            numero = numeros.stream().max(Integer::compare).get(); // obtiene el mayor numero de esa lista
+            numero++;
+        }
+        //Ahora se debe volver a pasar el numero a string con formato 0000011
+        if (numero<10){
+            numeroConcatenado="000000000"+String.valueOf(numero);
+        }else if (numero <100){
+            numeroConcatenado="00000000"+String.valueOf(numero);
+        }else if (numero <1000){
+            numeroConcatenado="0000000"+String.valueOf(numero);
+        }else if (numero <10000){
+            numeroConcatenado="000000"+String.valueOf(numero);
+        }else if (numero <100000){
+            numeroConcatenado="00000"+String.valueOf(numero);
+        }
+
+        return "";
     }
 }
